9d9e778d5c71941039a808f546c480c9fc8f3998
faiz ahmad <faizahmedkgn85@gmail.com>
2025-04-03 12:01:00 +0530

    added fontFamily-option,color option,boldoption,italicoption,highlight-option

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -4,6 +4,10 @@ import TaskItem from '@tiptap/extension-task-item';
 import TaskList from '@tiptap/extension-task-list';
 import Table from '@tiptap/extension-table';
 import TableCell from '@tiptap/extension-table-cell';
+import { Color } from '@tiptap/extension-color';
+import Highlight from '@tiptap/extension-highlight';
+import FontFamily from '@tiptap/extension-font-family';
+import TextStyle from '@tiptap/extension-text-style';
 import Image from '@tiptap/extension-image';
 import TableHeader from '@tiptap/extension-table-header';
 import TableRow from '@tiptap/extension-table-row';
@@ -48,8 +52,12 @@ export const Editor = () => {
             StarterKit,
             Image,
             ImageResize,
+            FontFamily,
+            TextStyle,
             Table,
             TableCell,
+            Color,
+            Highlight.configure({ multicolor: true }),
             TableHeader,
             TableRow,
             Underline,
