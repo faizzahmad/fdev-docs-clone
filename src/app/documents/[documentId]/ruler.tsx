1931ecb29deabe3dadf4418d3d52cb779af2b654
faiz ahmad <faizahmedkgn85@gmail.com>
2025-04-05 00:18:54 +0530

    added header at document page and header page

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -1,8 +1,80 @@
+import { useRef, useState } from 'react';
+import {FaCaretDown} from 'react-icons/fa';
 const markers = Array.from({ length: 83 }, (_, i) => i);
 export const Ruler = () => {
+    const [leftMargin, setLeftMargin] = useState(56);
+    const [rightMargin, setRightMargin] = useState(56);
+    const  [isDraggingLeft, setIsDraggingLeft] = useState(false);
+    const  [isDraggingRight, setIsDraggingRight] = useState(false);
+    const rulerRef = useRef<HTMLDivElement>(null);
+
+    const handelLeftMouseDown = () => {
+        setIsDraggingLeft(true);
+    };
+    const handelRightMouseDown = () => {
+        setIsDraggingRight(true);
+    };
+
+    const handelMouseMove = (e: React.MouseEvent) => {
+        const PAGE_WIDTH = 816;
+        const MINIMUM_SPACE = 100;
+        if((isDraggingLeft || isDraggingRight) && rulerRef.current){
+            const container = rulerRef.current.querySelector("#ruler-container");
+            if(container){
+                const containerRect = container.getBoundingClientRect();
+                const relativeX = e.clientX - containerRect.left;
+                const rowPosition = Math.max(0, Math.min(PAGE_WIDTH, relativeX));
+
+                if(isDraggingLeft){
+                    const maxLeftPosition = PAGE_WIDTH - rightMargin - MINIMUM_SPACE;
+                    const newLeftPosition = Math.min(rowPosition,maxLeftPosition);
+                    setLeftMargin(newLeftPosition); //TODO make collabrative
+                }else if(isDraggingRight){
+                    const maxRightPosition = PAGE_WIDTH - (leftMargin + MINIMUM_SPACE);
+                    const newRightPosition = Math.max(PAGE_WIDTH - rowPosition,0);
+                    const constrainedRightPosition = Math.min(newRightPosition, maxRightPosition);
+                    setRightMargin(constrainedRightPosition); 
+                }
+            }
+
+        }
+    }
+
+    const handelMouseUp = () => {
+        setIsDraggingLeft(false);
+        setIsDraggingRight(false);
+    };
+
+    const handelLeftDoubleClick = () => {
+        setLeftMargin(56);
+    }
+
+    const handelRightDoubleClick = () => {
+        setRightMargin(56);
+    }
+
     return (
-        <div className="h-6 border-b border-gray-300  flex items-end relative select-auto print-hidden">
-            <div id="ruler-container" className="max-w-[816px] mx-auto w-full h-full relative">
+        <div
+        ref = {rulerRef}
+        onMouseMove={handelMouseMove}
+        onMouseUp={handelMouseUp}
+        onMouseLeave={handelMouseUp}
+        className="w-[816px] mx-auto h-6 border-b border-gray-300  flex items-end relative select-auto print:hidden">
+            <div id="ruler-container" className="w-full h-full relative">
+                <Marker
+                position={leftMargin}
+                isLeft={true}
+                isDragging={isDraggingLeft}
+                onMouseDown={handelLeftMouseDown}
+                onDoubleClick={handelLeftDoubleClick}
+                />
+                 <Marker
+                position={rightMargin}
+                isLeft={false}
+                isDragging={isDraggingRight}
+                onMouseDown={handelRightMouseDown}
+                onDoubleClick={handelRightDoubleClick}
+                />
                 <div className="absolute inset-x-0 bottom-0 h-full">
                     <div className="relative h-full w-[816px]">
                         {
@@ -39,3 +111,38 @@ export const Ruler = () => {
         </div>
     );
 };
+
+
+interface MarkerPops {
+    position: number;
+    isLeft : boolean;
+    isDragging : boolean;
+    onMouseDown : (e: React.MouseEvent) => void;
+    onDoubleClick : (e: React.MouseEvent) => void;
+};
+
+const  Marker = ({ position, isLeft, isDragging, onMouseDown, onDoubleClick }: MarkerPops) => {
+    return(
+        <div className="absolute top-0 w-4 h-full cursor-ew-resize z-50 group -ml-2" style={{
+            [isLeft ? 'left' : 'right']: `${position}px`,
+        }}
+        onMouseDown={onMouseDown}
+        onDoubleClick={onDoubleClick}
+        >
+            <FaCaretDown className='absolute left-1/2 h-full  fill-blue-500 transform -translate-x-1/2'/>
+            <div
+            className='absolute left-1/2 top-4 transform -translate-x-1/2 '
+            style={{
+                height : "100vh",
+                width : "1px",
+                transform : "scaleX(0.5)",
+                backgroundColor : "#3b82f6",
+                display : isDragging ? "block" : "none",
+            }}
+            />
+        </div>
+    );
+}
+
+
+
---
+++ src/hooks/use-search-param.ts
@@ -0,0 +1,6 @@
+import {parseAsString,useQueryState} from 'nuqs';
+export function useSearchParam(key: string) {
+return useQueryState(key, 
+    parseAsString.withDefault('').withOptions({clearOnDefault : true}),
+);
+};
