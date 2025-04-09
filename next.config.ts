7213f7d734937fb58a2745fb4edb065297d2aea3
faiz ahmad <faizahmedkgn85@gmail.com>
2025-04-09 11:16:10 +0530

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: '/(.*)',
-        destination: 'https://fdev-docs-clone.vercel.app',
-        permanent: true,
-      },
-    ]
-  }
 };
-
 
 export default nextConfig;
